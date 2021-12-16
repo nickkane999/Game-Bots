14889e87d09e0483982bb41851ef0fc3e5002487
nickkane999 <nickkane999@gmail.com>
2021-12-15 22:49:32 -0500

    Update RebirthManager.py

--- NGU/actors/RebirthManager.py
+++ NGU/actors/RebirthManager.py
@@ -47,8 +47,7 @@ class RebirthManager:
                 if not adventure_zone_set:
                     self.bot.rebirth_manager.changeGearSlot("drop_rate_build")
                     self.bot.rebirth_manager.setAdventureZone("low")
-                    self.bot.rebirth_manager.assignAugments(self.augment)
-                    time.sleep(10)
+                    self.bot.rebirth_manager.assignAugments(10, self.augment)
                     self.bot.reclaimResource(True)
                     adventure_zone_set = True
                 self.bot.rebirth_manager.timeMachineCycle(10)
@@ -63,11 +62,9 @@ class RebirthManager:
                     self.bot.rebirth_manager.setDiggers()
                     new_adventure_zone_set = True
                 self.bot.reclaimResource(True)
-                self.bot.rebirth_manager.assignAugments(self.augment, True)
-                time.sleep(2)
+                self.bot.rebirth_manager.assignAugments(4, self.augment, True)
                 self.bot.reclaimResource(True)
-                self.bot.rebirth_manager.assignAugments(self.augment)
-                time.sleep(2)
+                self.bot.rebirth_manager.assignAugments(4, self.augment)
                 self.bot.reclaimResource(True)
                 self.bot.rebirth_manager.timeMachineCycle(12)
 
@@ -93,11 +90,9 @@ class RebirthManager:
             print("Finished Cycle 5")
             self.bot.reclaimResource(True)
             while time.time() - rebirth_start_time < 480:
-                self.bot.rebirth_manager.assignAugments(self.augment, True)
-                time.sleep(2)
-                self.bot.reclaimResource(True)
-                self.bot.rebirth_manager.assignAugments(self.augment)
-                self.bot.rebirth_manager.assignAugments("blood_4")
+                self.bot.rebirth_manager.assignAugments(3, self.augment, True)
+                self.bot.rebirth_manager.assignAugments(3, self.augment)
+                self.bot.rebirth_manager.setBlood("blood_4")
                 self.bot.rebirth_manager.nukeBoss()
                 time.sleep(5)
 
@@ -111,16 +106,20 @@ class RebirthManager:
                 self.bot.rebirth_manager.nukeBoss()
                 time.sleep(5)
 
+            self.bot.reclaimResource(True)
+            self.bot.reclaimResource(False)
+            self.bot.gear_manager.selectGear(self.gear["accessory"])
             self.attackBoss()
             
-            print("Finished cycle")
-            print(asdasdasdas)
+            print("Finished cycle. Restarting")
+            self.enterRebirth()
 
     def nukeBoss(self):
         self.game_ui.accessMenu("fight_boss")
         time.sleep(0.2)
         nuke = self.settings["fight_boss"]["nuke"]
         self.click(nuke)
+        print("Nuked boss")
 
     def attackBoss(self):
         self.game_ui.accessMenu("fight_boss")
@@ -134,10 +133,25 @@ class RebirthManager:
         #resource_build = 0, drop_rate_build = 1
         self.game_ui.accessMenu("inventory")
         time.sleep(0.2)
-        self.bot.gear_manager.assignLoadout(slot)    
+        self.bot.gear_manager.assignLoadout(slot)
+        print("Changed gear slot")
 
-    def assignAugments(self, augment, is_strong = False):
+    def assignAugments(self, set_time, augment, is_strong = False):
+        current_time = time.time()
+        loop_time = current_time
         self.game_ui.accessMenu("augmentation")
+        time.sleep(0.2)
+
+        while loop_time - current_time < set_time:
+            self.game_ui.accessMenu("wandos")
+            if dump != "energy":
+                self.click(magic_dump)
+            if dump != "magic":
+                self.click(energy_dump)
+            loop_time = time.time()
+            time.sleep(0.5)
+        print("Finished adding augments")
+
         self.bot.augmentation_manager.assignEnergy(augment, is_strong)
 
     def setAdventureZone(self, my_type = None):
@@ -155,6 +169,7 @@ class RebirthManager:
         else:
             for x in range(0, 20):
                 self.click(arrow_right)
+        print("Set adventure zone")
 
     def setDiggers(self, clear = True):
         self.game_ui.accessMenu("gold_diggers")
@@ -220,12 +235,14 @@ class RebirthManager:
         start_point = blood_settings["blood_start"]
         distance = blood_settings["distance"]
         self.click([start_point[0], start_point[1] + (blood_settings["info"][blood_type] * distance)])
+        print("Set blood")
 
     def enterRebirth(self):
         self.click(self.settings["rebirth"]["enter_menu"])
         self.click(self.settings["rebirth"]["rebirth_start"])
+        time.sleep(3)
         self.click(self.settings["rebirth"]["rebirth_confirm_start"])
-
+        print("Entered Rebirth")
 
     def click(self, point, rest_time = None):
         pyautogui.click(point[0], point[1])
